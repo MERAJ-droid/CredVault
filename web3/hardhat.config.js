2cf667d26ebc3f50b4623d4efa98ffd6ae83cb00
ghostfreak <mh4919124@gmail.com>
2025-02-08 01:51:12 +0530

    commit 9

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,22 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    fuji: {
+    avalancheFuji: {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
       chainId: 43113,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
-  
 };
 
